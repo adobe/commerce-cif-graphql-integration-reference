30f1e663711cf536441c8ccea34e82b27e54a1c3
Christophe Jelger <jelger@adobe.com>
2019-12-02 11:32:07 +0100

    CIF-1133 - Add example to store the schema using aio-lib (#9)
    - demonstrate how remote schemas can be cached with aio-lib-state

--- src/local/dispatcher.js
+++ src/local/dispatcher.js
@@ -16,7 +16,8 @@
 
 const magentoSchema = require('../resources/magento-schema-2.3.2.min.json');
 const { makeRemoteExecutableSchema, introspectSchema, mergeSchemas } = require('graphql-tools');
-const { graphql } = require('graphql');
+const { graphql, printSchema } = require('graphql');
+const libState = require('@adobe/aio-lib-state');
 
 const { Products, CategoryTree } = require('../common/Catalog.js');
 const ProductsLoader = require('../common/ProductsLoader.js');
@@ -24,33 +25,42 @@ const CategoryTreeLoader = require('../common/CategoryTreeLoader.js');
 const SchemaBuilder = require('../common/SchemaBuilder.js');
 const RemoteResolverFetcher = require('../common/RemoteResolverFetcher.js');
 
+// We cache the GraphQL schema in a global variable so that it is immediately available
+// if the same WARM container is reused in the Adobe I/O platform
 let cachedSchema = null;
 
-function resolve(args) {
+async function resolve(args) {
 
-    let remoteResolvers = [];
-    if (cachedSchema == null && args.remoteSchemas) {
-        // Get all resolver actions to fetch the remote schemas dynamically
-        remoteResolvers = Object.values(args.remoteSchemas).map(resolver => {
-            let fetcher = new RemoteResolverFetcher(resolver.action).fetcher;
-            return introspectSchema(fetcher).then(schema => {
-                return Promise.resolve({
-                    schema,
-                    fetcher,
-                    order: resolver.order
-                });
-            });
-        });
-    } else {
-        remoteResolvers.push(Promise.resolve({})); // Do nothing below
+    let remoteResolvers = null;
+    let state = null;        // The aio-lib-state object
+    let storeSchema = false; // If true, we will put the remote schemas in the aio-lib-state cache
+
+    // If the schema is not cached, we try to get the remote schemas from the aio-lib-state cache
+    if (cachedSchema == null && Number.isInteger(args['use-aio-cache'])) {
+        state = await libState.init();
+        remoteResolvers = await fetchRemoteSchemasFromCache(state);
     }
 
-    return Promise.all(remoteResolvers).then(remotes => {
+    // If the schema is not cached and we didn't get anything from the aio-lib-state cache,
+    // we prepare the remote fetchers to build the executable remote schemas
+    if (cachedSchema == null && args.remoteSchemas && remoteResolvers == null) {
+        remoteResolvers = prepareRemoteSchemaFetchers(args.remoteSchemas);
+        storeSchema = Number.isInteger(args['use-aio-cache']);
+    }
+    
+    // The schema is already available, we use a NOOP Promise for Promise.all()
+    if (remoteResolvers == null) {
+        remoteResolvers = [Promise.resolve({})]; // Do nothing below
+    }
+
+    return Promise.all(remoteResolvers).then(async remotes => {
 
         if (cachedSchema == null) {
             let remoteExecutableSchemas = [localSchema()];
 
             if (args.remoteSchemas) {
+                let cachedSchemas = [];
+
                 remotes.forEach(remote => {
                     let remoteExecutableSchema = makeRemoteExecutableSchema({
                         schema: remote.schema,
@@ -58,21 +68,30 @@ function resolve(args) {
                     });
                     remoteExecutableSchema.sortOrder = remote.order;
                     remoteExecutableSchemas.push(remoteExecutableSchema);
-                });
-            }
 
-            // Keep schema types with lowest order
-            let onTypeConflict = (left, right, info) => {
-                let diff = info.left.schema.sortOrder - info.right.schema.sortOrder;
-                return (diff <= 0) ? left : right;
-            };
+                    // We store the remote schemas in SDL form in the aio-lib-state cache
+                    if (storeSchema) {
+                        cachedSchemas.push({
+                            schema: printSchema(remote.schema),
+                            action: remote.action,
+                            order: remote.order
+                        });
+                    }
+                });
+
+                if (state && cachedSchemas.length > 0) {
+                    let ttl = args['use-aio-cache'];
+                    console.debug(`Trying to put schemas in aio-lib-state cache with ttl:${ttl} ...`);
+                    await state.put('schemas', cachedSchemas, {ttl});
+                }
+            }
 
             let finalSchema = mergeSchemas({
                 schemas: remoteExecutableSchemas,
                 onTypeConflict: onTypeConflict
             });
 
-            cachedSchema = finalSchema;
+            cachedSchema = finalSchema; // eslint-disable-line require-atomic-updates
         }
 
         // Passed to all resolver actions, can for example contain an authentication token
@@ -115,6 +134,58 @@ function resolve(args) {
     }).catch(error => {
         console.error(error);
     });
+}
+
+/**
+ * When merging schemas, this method keeps the data of the schema with lowest order.
+ * The parameters are automatically passed by the graphql-tools library.
+ */
+function onTypeConflict(left, right, info) {
+    let diff = info.left.schema.sortOrder - info.right.schema.sortOrder;
+    return (diff <= 0) ? left : right;
+}
+
+/**
+ * This method prepares the introspection calls that will be used to get all the remote schemas
+ * from all the remote actions.
+ * 
+ * @param {*} remoteSchemas The remote schemas configured in the dispatcher action.
+ */
+function prepareRemoteSchemaFetchers(remoteSchemas) {
+    // Get all resolver actions to fetch the remote schemas dynamically
+    return Object.values(remoteSchemas).map(resolver => {
+        console.debug(`--> Preparing remote schema fetcher for action ${resolver.action}`);
+        let fetcher = new RemoteResolverFetcher(resolver.action).fetcher;
+        return introspectSchema(fetcher).then(schema => {
+            return Promise.resolve({
+                schema,
+                fetcher,
+                order: resolver.order,
+                action: resolver.action
+            });
+        });
+    });
+}
+
+/**
+ * This method checks if the SDL remote schemas are available in the aio-lib-state cache.
+ * If successful, it returns an Array of remote resolvers that can be used to build the
+ * executable remote schemas. It returns null if nothing could be fetched from the cache.
+ */
+async function fetchRemoteSchemasFromCache(state) {
+    console.debug('Trying to get remote schemas from aio-lib-state cache ...');
+    let schemas = await state.get('schemas');
+    if (schemas) {
+        console.debug(`--> Got ${schemas.value.length} schemas from aio-lib-state cache`);
+        return schemas.value.map(obj => {
+            return Promise.resolve({
+                schema: obj.schema,
+                fetcher: new RemoteResolverFetcher(obj.action).fetcher,
+                order: obj.order
+            });
+        });
+    }
+    return null;
 }
 
 /**
@@ -174,4 +245,10 @@ function localSchema() {
     return schemaBuilder.build(10);
 }
 
+// Only exported for unit testing
+function cleanCachedSchema() {
+    cachedSchema = null;
+}
+
 module.exports.main = resolve;
+module.exports.cleanCacheSchema = cleanCachedSchema;
