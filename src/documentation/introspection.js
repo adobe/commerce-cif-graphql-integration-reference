9745b57e72406ca245f2ca689557c2a249d7a799
Christophe Jelger <jelger@adobe.com>
2020-02-27 14:18:18 +0100

    CIF-1293 - Prune Magento GraphQL schema with connector and components queries
    - automated the process to prune the Magento schema based on CIF GraphQL queries
    - created an action to deploy the pruned schema in I/O Runtime

---
+++ src/documentation/introspection.js
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ *
+ *    Copyright 2019 Adobe. All rights reserved.
+ *    This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License. You may obtain a copy
+ *    of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *    OF ANY KIND, either express or implied. See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ ******************************************************************************/
+
+'use strict';
+
+const magentoSchema = require('../resources/magento-schema-2.3.4.pruned.min.json');
+const { buildClientSchema, graphql } = require('graphql');
+
+function resolve(args) {
+
+    let schema = buildClientSchema(magentoSchema.data);
+
+    return graphql(schema, args.query, {}, {}, args.variables, args.operationName).then((response) => {
+        return {
+            body: response
+        }
+    });
+}
+
+module.exports.main = resolve;
---
+++ test/documentation/introspectionTest.js
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ *
+ *    Copyright 2019 Adobe. All rights reserved.
+ *    This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License. You may obtain a copy
+ *    of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *    OF ANY KIND, either express or implied. See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ ******************************************************************************/
+
+'use strict';
+
+const assert = require('chai').assert;
+const resolve = require('../../src/documentation/introspection.js').main;
+const { introspectionQuery } = require('graphql');
+
+describe('I/O Runtime action', () => {
+
+    describe('Integration Tests', () => {
+
+        // This test uses the generated Magento schema for 3rd-party integration
+
+        it('Execute introspection query and get pruned schema', () => {
+            return resolve({query: introspectionQuery}).then(result => {
+                assert.isUndefined(result.body.errors); // No GraphQL errors
+
+                let schema = result.body;
+
+                // Queries: cart, countries, customer, products, storeConfig, category
+                let queryType = schema.data.__schema.types.find(t => t.name == 'Query');
+                assert.equal(queryType.fields.length, 6);
+
+                // Mutations: a bunch of cart and customer related queries
+                let mutationType = schema.data.__schema.types.find(t => t.name == 'Mutation');
+                assert.equal(mutationType.fields.length, 17);
+            });
+        });
+    });
+});
